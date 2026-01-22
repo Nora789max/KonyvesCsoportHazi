c280830e5afe2fdee3c9868b30ba74fe9118fe7f
Nori <fubukishirou789@gmail.com>
2026-01-22 10:01:30 +0100

    athelyezes

---
+++ konyvesCsoport/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konyvesCsoport
+{
+    internal class Book
+    {
+        public  string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public int Year { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public Book() { }
+
+        public Book(string iSBN, string title, string author, int year, bool isAvailable)
+        {
+            ISBN = iSBN;
+            Title = title;
+            Author = author;
+            Year = year;
+            IsAvailable = isAvailable;
+        }
+
+        public override string? ToString()
+        {
+            return ($"ISBN: {ISBN} \t {Title} - {Author} ({Year}) - {(IsAvailable? "Available": "Non Available")} ");
+        }
+    }
+}
