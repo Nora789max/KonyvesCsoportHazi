3c41d488e8069772ae9cb09f6efc931befa71c51
Nori <fubukishirou789@gmail.com>
2026-01-23 17:33:00 +0100

    Add/remove book 1

---
+++ konyvesCsoport/Model/Library.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konyvesCsoport.Model
+{
+    public class Library
+    {
+
+        public List<Book> Books { get; set; }
+        public List<Member> Members { get; set; }
+
+        public Library() {
+            Books = new List<Book>();
+            Members = new List<Member>();
+        }
+
+        public void SaveToFile(string filename)
+        {
+            try
+            {
+                List<string> LinesToSave = new List<string>();
+                foreach (var book in Books)
+                {
+
+                    string sor = $"{book.ISBN};{book.Author};{book.Title};{book.Year};{book.IsAvailable}";
+                    LinesToSave.Add(sor);
+                }
+                File.WriteAllLines("DatabaseBooks.txt", LinesToSave, Encoding.UTF8);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hiba!");
+            }
+        }
+        public void AddBook (Book book)
+        {
+            Book newBook = new Book();
+            Console.WriteLine("<--- Könyv hozzáadása--->");
+            Console.WriteLine("Az ISBN kód: ");
+            newBook.ISBN = Console.ReadLine();
+            Console.WriteLine("A könyv címe: ");
+            newBook.Title =Console.ReadLine();
+            Console.WriteLine("A könyv szerzője: ");
+            newBook.Author = Console.ReadLine();
+            Console.WriteLine("A könyv kiadási éve: ");
+            newBook.Year = int.Parse(Console.ReadLine());
+            newBook.IsAvailable = true;
+
+            Books.Add(newBook);
+            this.SaveToFile("DatabaseBooks.txt");
+            Console.WriteLine("Hozzáadva!");
+        }
+
+        public void RemoveBook (string isbn)
+        {
+            Console.WriteLine("<--- Könyv törlése--->");
+            Console.WriteLine("Kérem a keresett ISBN kódot: ");
+            
+            Book bookToDelete = Books.FirstOrDefault(b => b.ISBN == isbn);
+
+                if (bookToDelete != null)
+                {
+                    Books.Remove(bookToDelete);
+                    SaveToFile("DatabaseBooks.txt");
+                    Console.WriteLine("Törölve");
+                }
+        }
+
+        public void FindBook(string isbn)
+        {
+            bool found = false;
+
+            foreach (Book book in Books)
+            {
+                if (book.ISBN == isbn)
+                {
+                    Console.WriteLine($"ISBN: {book.ISBN} \t {book.Title} - {book.Author} ({book.Year}) - {(book.IsAvailable ? "Available" : "Non Available")}");
+
+                    found = true; 
+                    break;
+                }
+                  
+            }
+            if (!found)
+            {
+                Console.WriteLine("Nincs ilyen!");
+            }
+        }
+
+        public void ListAvailableBooks()
+        {
+            foreach (var book in Books)
+            {
+                if (book.IsAvailable == true)
+                {
+                    Console.WriteLine($"ISBN: {book.ISBN}  | Cím: {book.Title} | Szerző: {book.Author} | Kiadás éve: {book.Year}");
+                }
+
+            }
+
+        }
+    }
+}
--- konyvesCsoport/Program.cs
+++ konyvesCsoport/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using konyvesCsoport.Model;
+using System;
+using System.Text;
+using static System.Reflection.Metadata.BlobBuilder;
+using System.IO;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static Library Konyvtar = new Library();
+        static void Admin()
+        {
+            Console.WriteLine("<--- Könyvtári Adatbázis --->");
+            Console.WriteLine("Admin, mit szeretnél csinálni?  Hozzáadni könyvet [1] vagy eltávolítani [2] ?");
+            int toDo = Convert.ToInt32(Console.ReadLine());
+            switch (toDo)
+            {
+                case 1:
+                    Konyvtar.AddBook(null);
+                    break;
+                case 2:
+                    Console.Write("Add meg a törölni kívánt ISBN-t: ");
+                    string isbn = Console.ReadLine();
+                    Konyvtar.RemoveBook(isbn);
+                    break;
+            }
+
+        }
+        static void User()
+        {
+            Console.WriteLine("<--- Könyvtári Adatbázis --->");
+            Console.WriteLine("Tag: ");
+            Console.WriteLine("Könyv keresése [1] vagy az elérhető könyvek listája [2]: ");
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<--- Könyvtári Adatbázis --->");
+            Console.WriteLine("Admin [1] vagy Tag[2]?");
+            int title = Convert.ToInt32(Console.ReadLine());
+            switch (title)
+            {
+                case 1:
+                    Admin();
+                    break;
+                case 2:
+                    User();
+                    break;
+            }
+        }
+    }
+}
+
+
+//admin hozzáad, töröl isbn alapján
+
+//tag kölcsönöz, visszahoz
+
